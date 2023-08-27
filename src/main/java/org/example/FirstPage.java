c6fa6e1244a834933526131898d04c39a8d854e3
CarlosGuzman01 <carlosguzmantejeda13@gmail.com>
2023-08-27 14:24:27 -0400

    done Setting the language to english

--- src/main/java/org/example/FirstPage.java
+++ src/main/java/org/example/FirstPage.java
@@ -1,10 +1,21 @@
+//the hyperlink that this class gets done: https://eticket.migracion.gob.do/
+
 package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 
 public class FirstPage {
 
     public static void  goToNextHyperLink(){
 
+        //going to the eticket website
         DriverSingleton.getInstance().get("https://eticket.migracion.gob.do");
+
+        //Setting the language to english
+        Select drop;
+        drop = new Select(DriverSingleton.getInstance().findElement(By.name("culture")));
+        drop.selectByVisibleText("ENGLISH");
 
 
 
