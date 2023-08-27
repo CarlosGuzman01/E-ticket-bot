364faaebce2dfb00dc246fefffda96255254b20f
CarlosGuzman01 <carlosguzmantejeda13@gmail.com>
2023-08-27 14:49:13 -0400

    done with FirstPage class

--- src/main/java/org/example/FirstPage.java
+++ src/main/java/org/example/FirstPage.java
@@ -12,24 +12,16 @@ public class FirstPage {
         //going to the eticket website
         DriverSingleton.getInstance().get("https://eticket.migracion.gob.do");
 
-        //Setting the language to english
-        Select drop;
-        drop = new Select(DriverSingleton.getInstance().findElement(By.name("culture")));
-        drop.selectByVisibleText("ENGLISH");
+        //Setting the language to english -- I commented out these lines of code because the eticket language menu is janky
+//        Select drop;
+//        drop = new Select(DriverSingleton.getInstance().findElement(By.name("culture")));
+//        drop.selectByVisibleText("ENGLISH");
 
         //click "e-ticket application"
         DriverSingleton.getInstance().findElement(By.id("request_ticket_btn")).click();
 
         //click "New eTicket"
         DriverSingleton.getInstance().findElement(By.cssSelector("button[class='swal2-confirm swal2-styled swal2-default-outline']")).click();
-
-
-
-
-
-
-
-
 
     }
 
